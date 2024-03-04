0e218bbe8d5d0948aba966a43b7e5ab2468c57cc
Cole Sutrick <papacole@Coles-MacBook-Pro-4.local>
2024-03-03 21:34:47 -0600

    Added basic client side setup, installed dependencies

---
+++ client/src/components/Navbar/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Navbar() {
+  return (
+    <nav className="w-full flex justify-center bg-red-300 py-2">
+      <p className="text-4xl font-bold tracking-wider">Navbar</p>
+    </nav>
+  );
+}
+
+export default Navbar;
---
+++ client/src/pages/HomeUser.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function HomeUser() {
+  return (
+    <div className="flex h-40 w-full items-center justify-center bg-blue-300">
+      <p className="text-4xl font-bold tracking-wider">Testing</p>
+    </div>
+  );
+}
+
+export default HomeUser;
